9d26de8758af42b3fcc5e0ec442c6e3f028d4040
Ronak Goyal <ronakgoyal1403@gmail.com>
2024-10-23 23:09:08 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import Header from "./header/Header";
+import Footer from "./footer/Footer";
+import Container from './container/Container';
+import Logo from "./Logo";
+import LogoutBtn from "./header/LogoutBtn";
+import RTE from './RTE'
+import Signup from './Signup';
+import Login from './Login'
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Select from './Select';
+
+
+export {Header,Footer,Container,Logo,LogoutBtn,RTE,Signup,Login,Button,PostCard,PostForm,AuthLayout,Input,Select};
